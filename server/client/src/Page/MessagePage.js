09090d6b13748e5a2e9b5631d02e6176e905734a
Prajwol <prajwolneupane68@gmail.com>
2022-11-25 16:49:52 +0545

    Second Commit

--- server/client/src/Page/MessagePage.js
+++ server/client/src/Page/MessagePage.js
@@ -1,5 +1,5 @@
 import { Avatar, Stack, Typography, useMediaQuery } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import LeftNavigationBar from '../Component/LeftNavigationBar'
 import ForwardToInboxOutlinedIcon from '@mui/icons-material/ForwardToInboxOutlined';
 
@@ -9,6 +9,7 @@ export default function MessagePage() {
     const image = 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80';
     const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4];
     const sm = useMediaQuery('(min-width:800px)');
+    const [userIndex, setUserIndex] = useState(null);
 
     return (
         <Stack sx={{ flexDirection: "row", width: "100%" }}>
@@ -29,14 +30,14 @@ export default function MessagePage() {
                                         flexDirection: "row", width: "90%", gap: "10px", padding: "6px 5%", alignItems: "center", cursor: "pointer", transition: "0.2s", ":hover": {
                                             backgroundColor: "whiteShade.light"
                                         }
-                                    }}>
+                                    }} onClick={() => { setUserIndex(indx) }}>
                                         {
                                             sm === true ? <> <Avatar src={image} sx={{ width: "55px", height: "55px" }} />
                                                 <Stack >
                                                     <Typography sx={{ fontSize: "15px" }} variant="h3">Prajwol Neupane</Typography>
                                                     <Typography sx={{ fontSize: "13px" }} variant="h3">OK</Typography>
                                                 </Stack>
-                                                <Stack sx={{ width: "8px", height: "8px", backgroundColor: "green", borderRadius: "4px", marginLeft: "25%",display:{md:"block",sm:"none",xs:"none"} }}></Stack></> : <>
+                                                <Stack sx={{ width: "8px", height: "8px", backgroundColor: "green", borderRadius: "4px", marginLeft: "25%", display: { md: "block", sm: "none", xs: "none" } }}></Stack></> : <>
                                                 <Stack >
                                                     <Typography sx={{ fontSize: "15px" }} variant="h3">Prajwol Neupane</Typography>
                                                     <Typography sx={{ fontSize: "13px" }} variant="h3">OK</Typography>
@@ -49,11 +50,34 @@ export default function MessagePage() {
 
                         </Stack>
                     </Stack>
-                    <Stack sx={{ width: "65%", alignItems: "center", justifyContent: "center" }}>
-                        <ForwardToInboxOutlinedIcon sx={{ fontSize: "70px" }} />
-                        <Typography variant='h5' sx={{ fontSize: "23px" }}>Your Messages</Typography>
-                        <Typography variant='h5' sx={{ fontSize: "18px" }}>Click on conversation to start chatting</Typography>
-                    </Stack>
+                    {
+                        userIndex === null ? <Stack sx={{ width: "65%", alignItems: "center", justifyContent: "center" }}>
+                            <ForwardToInboxOutlinedIcon sx={{ fontSize: "70px" }} />
+                            <Typography variant='h5' sx={{ fontSize: "23px" }}>Your Messages</Typography>
+                            <Typography variant='h5' sx={{ fontSize: "18px" }}>Click on conversation to start chatting</Typography>
+                        </Stack> :
+                            <Stack sx={{ width: "65%",padding:"10px 50px" }}>
+                                <Stack sx={{ flexDirection: "row", padding: "9px 20px", alignItems: "center", gap: "10px", borderBottom: "1px #9eaaba solid" }}>
+                                    <Avatar src={image} />
+                                    <Typography variant='h3' sx={{ fontSize: "15px" }}>Prajwol Neupane</Typography>
+                                </Stack>
+                                <Stack sx={{ flexDirection: "column", overflowY: "auto" }}>
+                                    <Stack  sx={{ alignItems: "flex-start" }}>
+                                        <Typography sx={{ backgroundColor: "blueShade.superlight", padding: "10px", borderRadius: "10px", maxWidth: "380px" }}>
+                                            Hello
+                                        </Typography>
+                                    </Stack>
+                                    <Stack sx={{ alignItems: "flex-end" }}>
+                                        <Typography sx={{ backgroundColor: "blueShade.superlight", padding: "10px", borderRadius: "10px", maxWidth: "350px", fontSize: "15px" }}>
+                                            HelloSearch for the keywords to learn more about each warning.
+                                            To ignore, add // eslint-disable-next-line to the line before.
+                                            Search for the keywords to learn more about each warning.
+                                            To ignore, add // eslint-disable-next-line to the line before.
+                                        </Typography>
+                                    </Stack>
+                                </Stack>
+                            </Stack>
+                    }
                 </Stack>
             </Stack>
         </Stack>
